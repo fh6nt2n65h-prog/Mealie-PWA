9aa64fc11e96ac3031b10dba2edc6a7c7687093e
Mealie PWA Committer <mealie@example.com>
2026-04-10 21:28:34 +0100

    chore: revert font back to Fraunces

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ export default {
                 olive: '#7B7A4B'
             },
             fontFamily: {
-                display: ['Alumni Sans Pinstripe', 'Georgia', 'serif'],
+                display: ['Fraunces', 'Georgia', 'serif'],
                 sans: ['Roboto Condensed', 'ui-sans-serif', 'system-ui', 'sans-serif']
             },
             boxShadow: {
